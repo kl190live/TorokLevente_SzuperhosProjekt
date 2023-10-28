1b5a9ef49cb0e490075fca92cb296d4b0e78c570
Levente Török <tlevi0409@gmail.com>
2023-10-28 11:36:22 +0200

    Miliárdos interfészt és a boszuallo osztalyt létrehoztam

---
+++ SzuperhosProjekt/Bosszuallo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzuperhosProjekt
+{
+    public abstract class Bosszuallo : ISzuperhos
+    {
+        private double szuperero;
+        private bool vanEGyengesege;
+
+        protected Bosszuallo(double szuperero, bool vanEGyengesege)
+        {
+            this.szuperero = szuperero;
+            this.vanEGyengesege = vanEGyengesege;
+        }
+
+        public double Szuperero { get => szuperero; set => szuperero = value; }
+        public bool VanEGyengesege { get => vanEGyengesege; set => vanEGyengesege = value; }
+
+
+        bool ISzuperhos.LegyoziE(ISzuperhos szuperhos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public abstract bool MegmentiAvilagot();
+
+        double ISzuperhos.MekkoraAzEreje()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"Vasember:Szupererő:{this.szuperero};{this.vanEGyengesege}";
+        }
+
+    }
+}
